4f8f60533ccd632c004d9edca882b866667cb4ab
ula <urszula.janiszkiewicz@gmail.com>
2023-03-23 21:10:01 +0100

    Change database connection file

--- backend/pool.ts
+++ backend/pool.ts
@@ -2,9 +2,9 @@ import mysql2 from 'mysql2/promise'
 export const pool =  mysql2.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'portfolio_shop',
-    password: 'maria11',
-    port: 4900,
+    database: 'database',
+    password: 'password',
+    port: 0000,
     namedPlaceholders: true
 });
 
